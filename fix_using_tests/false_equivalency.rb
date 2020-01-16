632a4cf75c9df8d08a97f6036d63e90f47f3616c
Halton Bagley <haltonba94@gmail.com>
2020-01-16 22:57:47 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,13 +1,15 @@
 
 
-def get_user_input
-  gets.chomp
-end
+
 
 def prompt_user
   puts "What would you like to do?"
   puts "1.) Eat a hamburger."
   puts "2.) Eat a ham."
+end
+
+def get_user_input
+  gets.chomp
 end
 
 def selection(num)
